7e518b0e93335966025c7a3558e70e0fa71dc13a
Ahmad Nurjalal <ahmad.nurjalal@accelbyte.net>
2021-12-31 08:40:50 +0700

    feat: add login functions

--- src/main/java/net/accelbyte/sdk/core/util/Helper.java
+++ src/main/java/net/accelbyte/sdk/core/util/Helper.java
@@ -4,9 +4,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 public class Helper {
-    private Helper(){
+
+    private Helper() {
 
     }
 
@@ -18,5 +23,31 @@ public class Helper {
             result.write(buffer, 0, length);
         }
         return result.toString(StandardCharsets.UTF_8.name());
+    }
+
+    public static String generateCodeVerifier() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] codeVerifier = new byte[32];
+        secureRandom.nextBytes(codeVerifier);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(codeVerifier);
+    }
+
+    public static String generateCodeChallenge(String codeVerifier) {
+        byte[] bytes;
+        bytes = codeVerifier.getBytes(StandardCharsets.US_ASCII);
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        if (md != null) {
+            md.update(bytes, 0, bytes.length);
+        }
+        byte[] digest = new byte[0];
+        if (md != null) {
+            digest = md.digest();
+        }
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
     }
 }
