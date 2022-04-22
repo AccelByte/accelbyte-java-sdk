196e535607e3ccb700a508dcf152b4baa72f6fba
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2022-04-22 16:45:02 +0700

    chore(core): operation - remove unnecessary jsonignore

--- src/main/java/net/accelbyte/sdk/core/Operation.java
+++ src/main/java/net/accelbyte/sdk/core/Operation.java
@@ -6,7 +6,6 @@
 
 package net.accelbyte.sdk.core;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import net.accelbyte.sdk.core.util.Helper;
@@ -22,98 +21,66 @@ import java.util.Map;
 @Getter
 @Setter
 public class Operation {
-
-    @JsonIgnore
     protected String url = "";
-
-    @JsonIgnore
     protected String method = "";
-
-    @JsonIgnore
     protected List<String> consumes = new ArrayList<>();
-
-    @JsonIgnore
     protected List<String> produces = new ArrayList<>();
-
     @Deprecated
-    @JsonIgnore
     protected String security = "";
-
-    @JsonIgnore
     protected String preferredSecurityMethod = "";
-
-    @JsonIgnore
     protected List<String> securities = new ArrayList<>();
-
-    @JsonIgnore
     protected Map<String, String> cookies = new HashMap<String, String>();
-
-    @JsonIgnore
     protected String locationQuery = "";
-
-    @JsonIgnore
     protected String authorizationOverride = "";
 
-    @JsonIgnore
-    protected static String createFullUrl(String url, String baseUrl, Map<String, String> pathParams,
-            Map<String, List<String>> queryParams, Map<String, String> collectionFormatMap)
-            throws UnsupportedEncodingException {
-        return Helper.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
-    }
-
-    @JsonIgnore
     public Map<String, String> getPathParams() {
         return new HashMap<>();
     }
 
-    @JsonIgnore
     public Map<String, List<String>> getQueryParams() {
         return new HashMap<>();
     }
 
-    @JsonIgnore
     public String getFullUrl(String baseUrl) throws UnsupportedEncodingException {
         return null;
     }
 
-    @JsonIgnore
     public Map<String, String> getCookies() {
         return cookies;
     }
 
-    @JsonIgnore
     public Object getBodyParams() {
         return null;
     }
 
-    @JsonIgnore
     public Map<String, Object> getFormDataParams() {
         return null;
     }
 
-    @JsonIgnore
     public boolean isValid() {
         return false;
     }
 
-    @JsonIgnore
     public Object parseResponse(int code, String contentTpe, InputStream payload)
             throws ResponseException, IOException {
         return null;
     }
 
-    @JsonIgnore
     public void handleEmptyResponse(int code, String contentTpe, InputStream payload)
             throws ResponseException, IOException {
 
     }
 
-    @JsonIgnore
     protected Map<String, String> getCollectionFormatMap() {
         return new HashMap<>();
     }
 
-    @JsonIgnore
+    protected String createFullUrl(String url, String baseUrl, Map<String, String> pathParams,
+            Map<String, List<String>> queryParams, Map<String, String> collectionFormatMap)
+            throws UnsupportedEncodingException {
+        return Helper.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
+    }
+
     protected String convertInputStreamToString(InputStream is) throws IOException {
         return Helper.convertInputStreamToString(is);
     }
--- src/test/java/net/accelbyte/sdk/core/CoreUnitTest.java
+++ src/test/java/net/accelbyte/sdk/core/CoreUnitTest.java
@@ -9,6 +9,8 @@ package net.accelbyte.sdk.core;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import net.accelbyte.sdk.core.util.Helper;
+
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -32,7 +34,7 @@ class CoreUnitTest {
         queryParams.put("city", Collections.singletonList("Seattle"));
         queryParams.put("color", Collections.singletonList("red"));
         String expected = "https://accelbyte.io/platform/namespace/accelbyte/userId/511132939439?color=red&city=Seattle";
-        String fullUrl = Operation.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
+        String fullUrl = Helper.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
         assertEquals(expected, fullUrl);
     }
 
@@ -46,7 +48,7 @@ class CoreUnitTest {
         collectionFormatMap.put("emails", "notDefinedType");
         collectionFormatMap.put("appIds", null);
         String expected = "https://accelbyte.io/platform/namespace/accelbyte/userId/511132939439?emails=test%40test.net,check%40check.io&appIds=1,2";
-        String fullUrl = Operation.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
+        String fullUrl = Helper.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
         assertEquals(expected, fullUrl);
     }
 
@@ -60,7 +62,7 @@ class CoreUnitTest {
         collectionFormatMap.put("emails", "csv");
         collectionFormatMap.put("appIds", "csv");
         String expected = "https://accelbyte.io/platform/namespace/accelbyte/userId/511132939439?emails=test%40test.net,check%40check.io&appIds=1,2";
-        String fullUrl = Operation.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
+        String fullUrl = Helper.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
         assertEquals(expected, fullUrl);
     }
 
@@ -74,7 +76,7 @@ class CoreUnitTest {
         collectionFormatMap.put("appIds", "multi");
         collectionFormatMap.put("skus", "multi");
         String expected = "https://accelbyte.io/platform/namespace/accelbyte/userId/511132939439?appIds=1&appIds=2&skus=3&skus=4";
-        String fullUrl = Operation.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
+        String fullUrl = Helper.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
         assertEquals(expected, fullUrl);
     }
 
@@ -88,7 +90,7 @@ class CoreUnitTest {
         collectionFormatMap.put("emails", "pipes");
         collectionFormatMap.put("appIds", "pipes");
         String expected = "https://accelbyte.io/platform/namespace/accelbyte/userId/511132939439?emails=test%40test.net|check%40check.io&appIds=1|2";
-        String fullUrl = Operation.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
+        String fullUrl = Helper.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
         assertEquals(expected, fullUrl);
     }
 
@@ -102,7 +104,7 @@ class CoreUnitTest {
         collectionFormatMap.put("emails", "tsv");
         collectionFormatMap.put("appIds", "tsv");
         String expected = "https://accelbyte.io/platform/namespace/accelbyte/userId/511132939439?emails=test%40test.net\tcheck%40check.io&appIds=1\t2";
-        String fullUrl = Operation.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
+        String fullUrl = Helper.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
         assertEquals(expected, fullUrl);
     }
 
@@ -116,7 +118,7 @@ class CoreUnitTest {
         collectionFormatMap.put("emails", "ssv");
         collectionFormatMap.put("appIds", "ssv");
         String expected = "https://accelbyte.io/platform/namespace/accelbyte/userId/511132939439?emails=test%40test.net check%40check.io&appIds=1 2";
-        String fullUrl = Operation.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
+        String fullUrl = Helper.createFullUrl(url, baseUrl, pathParams, queryParams, collectionFormatMap);
         assertEquals(expected, fullUrl);
     }
 }
