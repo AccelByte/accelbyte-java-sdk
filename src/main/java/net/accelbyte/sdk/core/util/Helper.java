1cba4c1e50f0474f4144cd5c2e052834e935606d
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2022-04-22 07:50:22 +0700

    chore(core): operation - move createFullUrl logic to helper

--- src/main/java/net/accelbyte/sdk/core/util/Helper.java
+++ src/main/java/net/accelbyte/sdk/core/util/Helper.java
@@ -13,8 +13,10 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -23,6 +25,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -34,6 +37,102 @@ public class Helper {
 
     private Helper() {
 
+    }
+
+    public static String createFullUrl(String url, String baseUrl, Map<String, String> pathParams,
+            Map<String, List<String>> queryParams, Map<String, String> collectionFormatMap)
+            throws UnsupportedEncodingException {
+        final String ENC = "UTF-8";
+        StringBuilder result = new StringBuilder();
+
+        if (url == null) {
+            throw new IllegalArgumentException("Path cannot be null");
+        }
+
+        if (baseUrl == null) {
+            throw new IllegalArgumentException("Base URL cannot be null");
+        }
+
+        // base url
+        if (baseUrl.endsWith("/")) {
+            baseUrl = baseUrl.substring(0, baseUrl.length() - 1);
+        }
+        result.append(baseUrl);
+
+        // path params
+        if (pathParams.size() > 0) {
+            for (Map.Entry<String, String> pathParam : pathParams.entrySet()) {
+                url = url.replace("{" + pathParam.getKey() + "}", URLEncoder.encode(pathParam.getValue(), ENC));
+            }
+        }
+        result.append(url);
+
+        // query params
+        if (queryParams.size() > 0) {
+            result.append("?");
+            StringBuilder queryParamBuilder = new StringBuilder();
+            Iterator<Map.Entry<String, List<String>>> queryParamItr = queryParams.entrySet().iterator();
+            while (queryParamItr.hasNext()) {
+                Map.Entry<String, List<String>> qParams = queryParamItr.next();
+                if (qParams.getValue() == null) {
+                    continue;
+                }
+                if (qParams.getValue().size() > 1) {
+                    String collectionFormat = collectionFormatMap.get(qParams.getKey());
+                    StringBuilder collectionBuilder = new StringBuilder();
+                    if (collectionFormat != null && collectionFormat.equals("multi")) {
+                        Iterator<String> valItr = qParams.getValue().iterator();
+                        while (valItr.hasNext()) {
+                            String val = valItr.next();
+                            if (val != null) {
+                                collectionBuilder
+                                        .append(qParams.getKey())
+                                        .append("=")
+                                        .append(URLEncoder.encode(val, ENC));
+                                if (valItr.hasNext()) {
+                                    collectionBuilder.append("&");
+                                }
+                            }
+                        }
+                    } else {
+                        String delimiter = null;
+                        if (collectionFormat == "csv") {
+                            delimiter = ",";
+                        } else if (collectionFormat == "ssv") {
+                            delimiter = " ";
+                        } else if (collectionFormat == "tsv") {
+                            delimiter = "\t";
+                        } else if (collectionFormat == "pipes") {
+                            delimiter = "|";
+                        } else {
+                            delimiter = ","; // Collection format CSV by default
+                        }
+                        collectionBuilder
+                                .append(qParams.getKey())
+                                .append("=");
+                        Iterator<String> val = qParams.getValue().iterator();
+                        while (val.hasNext()) {
+                            collectionBuilder.append(URLEncoder.encode(val.next(), ENC));
+                            if (val.hasNext()) {
+                                collectionBuilder.append(delimiter);
+                            }
+                        }
+
+                    }
+                    queryParamBuilder.append(collectionBuilder);
+                } else {
+                    queryParamBuilder.append(qParams.getKey());
+                    queryParamBuilder.append("=");
+                    queryParamBuilder.append(URLEncoder.encode(qParams.getValue().get(0), ENC));
+                }
+                if (queryParamItr.hasNext()) {
+                    queryParamBuilder.append("&");
+                }
+
+            }
+            result.append(queryParamBuilder);
+        }
+        return result.toString();
     }
 
     public static String convertInputStreamToString(InputStream is) throws IOException {
