cac27523676a0c55d4727c987ab7b63a532917c1
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2022-06-16 11:43:06 +0700

    feat(core): make DefaultTokenRepository constructor public

--- src/main/java/net/accelbyte/sdk/core/repository/DefaultTokenRepository.java
+++ src/main/java/net/accelbyte/sdk/core/repository/DefaultTokenRepository.java
@@ -9,7 +9,7 @@ package net.accelbyte.sdk.core.repository;
 public class DefaultTokenRepository implements TokenRepository {
     private String accessToken = null;
 
-    protected DefaultTokenRepository() {
+    public DefaultTokenRepository() {
     }
 
     private static final class InstanceHolder {
