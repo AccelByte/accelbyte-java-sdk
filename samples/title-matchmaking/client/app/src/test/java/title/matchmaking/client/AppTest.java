b88ab6c57e70d8b93484be03833ad7820be87908
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2022-08-24 23:05:00 +0000

    feat(samples): title matchmaking sample app

---
+++ samples/title-matchmaking/client/app/src/test/java/title/matchmaking/client/AppTest.java
@@ -0,0 +1,8 @@
+package titlematchmaking.client;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+}
---
+++ samples/title-matchmaking/lambda/TitleMatchmakingFunction/src/test/java/titlematchmaking/lambda/AppTest.java
@@ -0,0 +1,11 @@
+package titlematchmaking.lambda;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+
+}
