04e271d9b1751f7a91ea4fd8520f1d9f01494f75
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2022-11-23 10:11:47 +0700

    feat(samples): getting started project

---
+++ samples/getting-started/app/src/main/java/getting/started/App.java
@@ -0,0 +1,43 @@
+package getting.started;
+
+import net.accelbyte.sdk.api.iam.models.OauthmodelCountryLocationResponse;
+import net.accelbyte.sdk.api.iam.operations.o_auth2_0_extension.GetCountryLocationV3;
+import net.accelbyte.sdk.api.iam.wrappers.OAuth20Extension;
+import net.accelbyte.sdk.core.AccelByteSDK;
+import net.accelbyte.sdk.core.client.OkhttpClient;
+import net.accelbyte.sdk.core.repository.DefaultConfigRepository;
+import net.accelbyte.sdk.core.repository.DefaultTokenRepository;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Create default HTTP client, token repository, and config repository instance
+
+        OkhttpClient httpClient = new OkhttpClient();
+        DefaultTokenRepository tokenRepository = new DefaultTokenRepository();
+        DefaultConfigRepository configRepository = new DefaultConfigRepository();
+
+        // Create SDK instance
+
+        AccelByteSDK sdk = new AccelByteSDK(
+                httpClient,
+                tokenRepository,
+                configRepository);
+
+        // Login using client credentials
+
+        boolean isLoginOk = sdk.loginClient();
+
+        if (!isLoginOk) {
+            System.exit(1); // Login failed
+        }
+
+        // Call an AccelByte Cloud endpoint e.g. GetCountryLocationV3
+
+        OAuth20Extension wrapper = new OAuth20Extension(sdk);
+        GetCountryLocationV3 operation = GetCountryLocationV3.builder()
+                .build();
+        OauthmodelCountryLocationResponse response = wrapper.getCountryLocationV3(operation);
+
+        System.out.println(response.getCountryName());
+    }
+}
