ccee347e0e33e03a7d70701b9a328c73f879c249
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2023-01-16 14:03:22 +0700

    chore: bump to version 0.28 for ab cloud 3.40

--- src/main/java/net/accelbyte/sdk/core/SDKInfo.java
+++ src/main/java/net/accelbyte/sdk/core/SDKInfo.java
@@ -14,7 +14,7 @@ public class SDKInfo {
   private static final SDKInfo instance = new SDKInfo();
 
   private String sdkName = "AccelByteJavaSDK";
-  private String sdkVersion = "0.27.0";
+  private String sdkVersion = "0.28.0";
 
   private SDKInfo() {}
 
