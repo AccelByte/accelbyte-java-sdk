b986cf402afbf09efa248c75e32148b64c3e1932
nanangarsyad <nanang@accelbyte.net>
2023-12-11 11:35:57 +0700

    chore: release versio 0.52.1

--- src/main/java/net/accelbyte/sdk/core/SDKInfo.java
+++ src/main/java/net/accelbyte/sdk/core/SDKInfo.java
@@ -14,7 +14,7 @@ public class SDKInfo {
   private static final SDKInfo instance = new SDKInfo();
 
   private String sdkName = "AccelByteJavaSDK";
-  private String sdkVersion = "0.52.0";
+  private String sdkVersion = "0.52.1";
 
   private SDKInfo() {}
 
