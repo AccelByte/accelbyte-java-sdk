07a28628e799f4120a4d605e66c014ce2febc4b7
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2024-07-09 03:29:46 +0700

    test(integration): session history - remove xray

--- src/test/java/net/accelbyte/sdk/integration/TestIntegrationServiceSessionHistory.java
+++ src/test/java/net/accelbyte/sdk/integration/TestIntegrationServiceSessionHistory.java
@@ -6,20 +6,13 @@
 
 package net.accelbyte.sdk.integration;
 
-import static java.time.temporal.ChronoUnit.DAYS;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import net.accelbyte.sdk.api.sessionhistory.models.ApimodelsGameSessionDetailQueryResponse;
 import net.accelbyte.sdk.api.sessionhistory.models.ApimodelsMatchmakingDetailQueryResponse;
-import net.accelbyte.sdk.api.sessionhistory.models.ApimodelsXRayMatchMatchmakingQueryResponse;
 import net.accelbyte.sdk.api.sessionhistory.operations.game_session_detail.AdminQueryGameSessionDetail;
 import net.accelbyte.sdk.api.sessionhistory.operations.game_session_detail.AdminQueryMatchmakingDetail;
-import net.accelbyte.sdk.api.sessionhistory.operations.x_ray.QueryTotalMatchmakingMatch;
 import net.accelbyte.sdk.api.sessionhistory.wrappers.GameSessionDetail;
-import net.accelbyte.sdk.api.sessionhistory.wrappers.XRay;
 import org.junit.jupiter.api.*;
 
 @Tag("test-integration")
@@ -32,44 +25,6 @@ public class TestIntegrationServiceSessionHistory extends TestIntegration {
   }
 
   @Test
-  @Order(1)
-  @Disabled
-  public void TotalMatchmakingMatchTests() throws Exception {
-    if (isUsingAGSStarter()) {
-      // Temporarily disabled in AGS Starter due to issue in session service.
-      return;
-    }
-
-    final XRay xRayWrapper = new XRay(sdk);
-
-    // Format: 2024-04-05T03:42:49Z
-    // https://development.accelbyte.io/sessionhistory/apidocs/#/XRay/queryTotalMatchmakingMatch
-    DateTimeFormatter formatter =
-        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'").withZone(ZoneOffset.UTC);
-
-    Instant now = Instant.now();
-    final String endDate = formatter.format(now);
-    final String startDate = formatter.format(now.minus(10, DAYS));
-
-    final QueryTotalMatchmakingMatch queryTotalMatchmakingMatchModel =
-        QueryTotalMatchmakingMatch.builder()
-            .namespace(this.namespace)
-            .endDate(endDate)
-            .startDate(startDate)
-            .build();
-
-    // Get total matchmaking match
-    ApimodelsXRayMatchMatchmakingQueryResponse response =
-        xRayWrapper.queryTotalMatchmakingMatch(queryTotalMatchmakingMatchModel);
-    assertNotNull(response);
-
-    if ((response != null) && (response.getData() != null)) {
-      assertTrue(response.getData().size() >= 0);
-    }
-  }
-
-  @Test
-  @Order(2)
   public void GameSessionTests() throws Exception {
     final GameSessionDetail gameSessionDetailWrapper = new GameSessionDetail(sdk);
 
